7361f3752927d0fceb6e745e17b185140a29c352
SnailTDang <88518610+SnailTDang@users.noreply.github.com>
2022-05-30 14:58:10 +0700

    Update scrollMenu.js

--- app/controllers/scrollMenu.js
+++ app/controllers/scrollMenu.js
@@ -5,12 +5,12 @@ export default function scrollMenu() {
             document.body.scrollTop > 150 ||
             document.documentElement.scrollTop > 150
         ) {
-            headerMenu.classList.add("menu-scroll");
+//             headerMenu.classList.add("menu-scroll");
             for (let menuItem of menuItems) {
                 menuItem.querySelector("span").style.display = "none";
             }
         } else {
-            headerMenu.classList.remove("menu-scroll");
+//             headerMenu.classList.remove("menu-scroll");
             for (let menuItem of menuItems) {
                 menuItem.querySelector("span").style.display = "inline-block";
             }
